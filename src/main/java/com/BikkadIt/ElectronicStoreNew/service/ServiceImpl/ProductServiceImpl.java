6a50e7a2828a959ad51fc78f03cb6d7d86f34ab7
raut sagar <sagarraut332@gmail.com>
2023-12-09 19:36:22 +0530

    changes done into logger.xml file line no 20 make 381 to 21

---
+++ src/main/java/com/BikkadIt/ElectronicStoreNew/service/ServiceImpl/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.BikkadIt.ElectronicStoreNew.service.ServiceImpl;
+
+import com.BikkadIt.ElectronicStoreNew.Dto.CategoryDto;
+import com.BikkadIt.ElectronicStoreNew.Dto.ProductDto;
+import com.BikkadIt.ElectronicStoreNew.service.serviceI.ProductServiceI;
+
+public class ProductServiceImpl implements ProductServiceI {
+    @Override
+    public ProductDto createProduct(ProductDto productDto) {
+        return null;
+    }
+
+    @Override
+    public ProductDto getAllProduct() {
+        return null;
+    }
+
+    @Override
+    public ProductDto getSingleProduct(String productId) {
+        return null;
+    }
+
+    @Override
+    public ProductDto updateProduct(String productId, CategoryDto categoryDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(String productId) {
+
+    }
+}
